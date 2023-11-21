e78c723658acedae6800a48af868314991c80e7e
Alfre Chazarreta <alfre.czta@gmail.com>
2023-11-21 19:41:17 -0300

    commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+const port = process.env.PORT || 3001
+app.listen(port, ()=> {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
 app.get('/', (req,res)=>{
